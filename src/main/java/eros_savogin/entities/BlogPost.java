54140c7bb9418412ccbacdb0dae422deb31f119d
Eros Savogin <eros.sav@gmail.com>
2024-02-03 16:08:11 +0100

    update comment

--- src/main/java/eros_savogin/entities/BlogPost.java
+++ src/main/java/eros_savogin/entities/BlogPost.java
@@ -17,7 +17,8 @@ public class BlogPost {
     private User user;    // <---- FOREIGN KEY, verso User         // devo essere sicuro che sia collegato ad uno User,
                                                                    // facendo così non c'è rischio che io crei un
                                                                    // blogpost non associato a un utente
-                                                                   // qui sto creando una nuova colonna quindi
+                                                                   // qui sto creando una nuova colonna, la faccio qui perchè sarà collegata ad un unico user,
+                                                                   // se facessi il contrario mi ritroverei una lista di chiavi, che è un problema
     @ManyToMany
     @JoinTable(name = "blogs_categories",
               joinColumns = @JoinColumn(name = "blogPost_id"),
